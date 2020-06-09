bad69f6db7307cea50e249059b262883417a5fe9
christianbendtsen <44088367+christianbendtsen@users.noreply.github.com>
2020-06-10 01:47:56 +0200

    Add files via upload

---
+++ FirstHalf.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class FirstHalf {
+
+    public String firstHalf(String str) {
+        return str.substring(0, str.length() >> 1);
+    }
+}
---
+++ MakePi.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class MakePi {
+    public int[] makePi() {
+        int [] arr = new int [] {3, 1, 4};
+        return arr;
+    }
+}
